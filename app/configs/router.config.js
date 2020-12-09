539770783568e8937d2c3f84053ad2f8861a1691
qiu bo <qiubo2@lenovo.com>
2020-12-09 22:15:33 +0800

    fx

--- app/configs/router.config.js
+++ app/configs/router.config.js
@@ -3,32 +3,16 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect,  useLocation } 
 import Error from '@components/Index/error'
 import Login from '@components/Index/login'
 import Index from '@components/Index/index'
-<<<<<<< HEAD
-import MainHeaders from '@components/departments/headers'
-=======
 import MainHeaders from '@components/departments/mainHeaders'
->>>>>>> 4065bb13c51eec25f258657d771460be14a75bd9
 
 
 export default function RouteConfig() {
     return (
         <Router>
             <Switch>
-<<<<<<< HEAD
-                <Route path="/" exact>
-                    <Index />
-                </Route>
-                <Route path="/login">
-                    <Index />
-                </Route>
-                <Route path="*">
-                    <Error />
-                </Route>
-=======
                 <Route path="/" exact component={Index} />
                 <Route path="/login" component={MainHeaders} />
                 <Route path="*" component={Error} />
->>>>>>> 4065bb13c51eec25f258657d771460be14a75bd9
             </Switch>
         </Router>
     )
